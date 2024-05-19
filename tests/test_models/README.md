3157762ceda6ff34d6e4ff38371be56884657b10
teresiawairimu <teresiawairimu06@gmail.com>
2024-05-19 12:26:03 -0700

    task 3

---
+++ tests/test_models/README.md
@@ -0,0 +1,2 @@
+#Test Suite for Models
+This directory contains a collection of tests to verify the functionality of the classes and functions defined in the 'models' directory.
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,22 @@
+import unittest 
+from models.base_model import BaseModel
+from datetime import datetime
+import uuid
+
+
+class TestBaseModel(unittest.TestCase):
+    """This class contains the Unit tests for the BaseModel class."""
+
+    def test_initialization(self):
+        """Tests whether a new BaseModel instance is properly initialized"""
+        new_model = BaseModel()
+        self.assertIsInstance(new_model, BaseModel)
+        self.assertIsInstance(new_model.id, str)
+        self.assertIsInstance(uuid.UUID(new_model.id), uuid.UUID)
+        self.assertEqual(new_model.name, "")
+        self.assertIsInstance(new_model.created_at, datetime)
+        self.assertEqual(new_model.updated_at, new_model.created_at)
+
+
+if __name__ == '__main__':
+    unittest.main()
