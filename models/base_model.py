b7cdd9157c7e1bdc5b049d6f002c871fe09004bd
teresiawairimu <teresiawairimu06@gmail.com>
2024-05-21 08:43:35 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -15,19 +15,24 @@ class BaseModel:
         updated_at(datetime): The datetime when the instance was last updated
     """
 
-    def __init__(self, name=""):
+    def __init__(self, *args, **kwargs):
         """Initializes a new BaseModel object
 
         Args:
-            id(str): Unique identifier of the instance
-            name(str): Name of the instance created
-            created_at(datetime): The datetime when the instance was created
-            updated_at(datetime): Datetime when the instance was last updated
+            args: allows for any number of positional arguments
+            kwargs: allows for any number of keyword arguments
         """
-        self.id = str(uuid.uuid4())
-        self.name = name
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'update_at':
+                    value = datetime.fromisoformat(value)
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.name = ""
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def save(self):
         """Updates updated-at with the current datetime"""
