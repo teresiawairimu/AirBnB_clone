f0a0a53f88a4587f4acfcc9cb69dc4587ca69854
teresiawairimu <teresiawairimu06@gmail.com>
2024-05-21 11:24:16 -0700

    task 5

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """create a unique FileStorage instance"""
 
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
