183ddccbc9b64df26c89bde4b7e06a089f320e6b
teresiawairimu <teresiawairimu06@gmail.com>
2024-05-21 18:02:18 -0700

    task 9

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 
 from models.base_model import BaseModel
 
+
 class Place(BaseModel):
     """class Place inherits from BaseModel
 
@@ -30,4 +31,3 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
-
